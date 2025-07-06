fb4a0bf83c968e0b227967aa7e77fb24288eea84
RyRose <RyRose@users.noreply.github.com>
2025-07-05 22:10:53 -0500

    feat: 02 solution

--- 2023/02/p2/main.scala
+++ 2023/02/p2/main.scala
@@ -1,33 +1,33 @@
 import scala.util.Using
 import scala.io.Source
+import scala.collection.mutable
 
 @main
-def main1(filepath: String): Unit = {
+def main2(filepath: String): Unit = {
   val integerRE = "[0-9]+".r
-  val letterRE = "[a-z]+".r
-  val map = Map(
-    "red" -> 12,
-    "green" -> 13,
-    "blue" -> 14
-  )
-  var total = 0
+  val letterRE  = "[a-z]+".r
+  var total     = 0
   Using(Source.fromFile(filepath)) { source =>
     for (line <- source.getLines()) {
       val List(game, bag) = line.split(":").toList
-      val gameIndex = integerRE.findFirstIn(game).get.toInt
-      var valid = true
+      val gameIndex       = integerRE.findFirstIn(game).get.toInt
+      var map             = mutable.Map(
+        "red"   -> 0,
+        "green" -> 0,
+        "blue"  -> 0
+      )
       bag.split(";").foreach { grab =>
         grab.split(",").foreach { item =>
           val itemCount = integerRE.findFirstIn(item).get.toInt
-          val word = letterRE.findFirstIn(item).get
-          if (map.get(word).get < itemCount) {
-            valid = false
-          }
+          val word      = letterRE.findFirstIn(item).get
+          map(word) = math.max(map(word), itemCount)
         }
       }
-      if (valid) {
-        total += gameIndex
+      var power = 1
+      map.values.foreach { count =>
+        power *= count
       }
+      total += power
     }
   }.recover { case e: Exception =>
     println(s"An error occurred: ${e.getMessage}")
